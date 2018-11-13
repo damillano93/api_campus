9c5225e2ab6eb703be67baeed18d4eaaa6770b43
david millan <damillano93@gmail.com>
2018-11-13 10:49:37 -0500

    agrega organizacion

--- organizacion_crud.sh
+++ organizacion_crud.sh
@@ -1,4 +1,4 @@
 killall organizacion_crud
 cd $GOPATH/src/github.com/udistrital/organizacion_crud/
-firefox --new-tab http://localhost:8097/swagger
+google-chrome http://localhost:8097/swagger
 ORGANIZACION_CRUD__HTTP_PORT=8097 ORGANIZACION_CRUD__DB_USER=postgres ORGANIZACION_CRUD__DB_PASS=password ORGANIZACION_CRUD__DB_URL=localhost ORGANIZACION_CRUD__DB_NAME=local ORGANIZACION_CRUD__SCHEMA=core_new bee run -gendoc=true 
